24611660212f5ec3411a68995350e36affa698a6
lm <leo.mangutov@gmail.com>
2019-03-29 11:46:32 -0700

    figured out how to show my nav bar on page

--- go-pro/src/app/app.module.ts
+++ go-pro/src/app/app.module.ts
@@ -4,14 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from '../app/nav-bar/nav-bar.component';
-import { NavBar2Component } from '../app/nav-bar2/nav-bar2.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    NavBar2Component
+    NavBarComponent
   ],
   imports: [
     BrowserModule
