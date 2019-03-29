834c322fc8d9668665029055d5e2b9b515a9bce7
lm <leo.mangutov@gmail.com>
2019-03-29 14:22:57 -0700

    add gif video, and content to main page

--- go-pro/src/app/app.module.ts
+++ go-pro/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from '../app/nav-bar/nav-bar.component';
 import { NavBar2Component } from './nav-bar2/nav-bar2.component';
+import { MainVideoComponent } from './main-video/main-video.component';
+import { Content1Component } from './content1/content1.component';
 
 
 
@@ -12,7 +14,9 @@ import { NavBar2Component } from './nav-bar2/nav-bar2.component';
   declarations: [
     AppComponent,
     NavBarComponent,
-    NavBar2Component
+    NavBar2Component,
+    MainVideoComponent,
+    Content1Component
   ],
   imports: [
     BrowserModule
