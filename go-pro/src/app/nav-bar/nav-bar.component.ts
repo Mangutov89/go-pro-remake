fb2e110cefccb9a2937d8eccb47e0261df2cfee1
lm <leo.mangutov@gmail.com>
2019-03-29 11:03:10 -0700

    nav bar wont show up on the page, trying to fix this

--- go-pro/src/app/nav-bar/nav-bar.component.ts
+++ go-pro/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
-
+export class NavBarComponent {
+  heading = 'go-pro';
   constructor() { }
 
   ngOnInit() {
